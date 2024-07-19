05676f090883e155a6ee07bb13a25736d791f247
tomyo <tomyhayes@gmail.com>
2024-07-19 03:07:39 -0300

    Show event openend when is the only one rendered
    What a mess i'm doing.

--- components/event-entry.js
+++ components/event-entry.js
@@ -93,7 +93,7 @@ customElements.define(
       }" loading="lazy" alt="Evento en ${
         event["Localidad"]
       } el ${formatDateString(event["Comienzo"])}">
-        <details>
+        <details ${this.open ? "open" : ""}>
           <summary>
 
           <div class="info">
@@ -112,6 +112,22 @@ customElements.define(
 
         </details>
       `;
+    }
+    static observedAttributes = ["open", "order"];
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (name === "open") {
+        this.open = !!newValue;
+        const details = this.querySelector("details");
+        if (details) details.open = !!newValue;
+      }
+    }
+
+    handleEvent(event) {
+      if (event.type === "toggle") {
+        if (event.target.open != this.open) this.open = event.target.open;
+        // this.toggleAttribute("open", event.target.open);
+      }
     }
 
     renderButtons() {
@@ -173,12 +189,6 @@ customElements.define(
       )}"><svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg></a>`;
 
       return htmlString;
-    }
-
-    handleEvent(event) {
-      if (event.type === "toggle") {
-        this.toggleAttribute("open", event.target.open);
-      }
     }
   }
 );
