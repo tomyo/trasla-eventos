ddaf49a8f0dd4e6d1dee35c53e1c780a86e7fb64
tomyo <tomyhayes@gmail.com>
2024-07-19 13:54:37 -0300

    Implement native share/copy to clipboard on share button

--- components/event-entry.js
+++ components/event-entry.js
@@ -1,3 +1,5 @@
+import "./share-url.js";
+
 /**
  * @typedef {Object} EventResponse - The response from the Google Form.
  * @property {string} MarcaTemporal - The timestamp of the event.
@@ -182,11 +184,17 @@ customElements.define(
       const slug = slugify(
         event["Localidad"] + " " + formatDateString(event["Comienzo"])
       );
-      htmlString += /*html*/ `<a target="_blank" part="button" href="${
-        location.origin
-      }?q=${encodeURI(
-        slug
-      )}"><svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg></a>`;
+
+      const shareAction = navigator.share ? "share" : "clipboard";
+      const sharedMsg = navigator.share ? "Compartido" : "Link copiado";
+      const shareUrl = `${location.origin}?q=${encodeURI(slug)}`;
+
+      htmlString += /*html*/ `
+        <share-url action="${shareAction}" text-success="${sharedMsg}" url="${shareUrl}">
+        <button part="button">
+          <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg>
+        </button>
+      </share-url>`;
 
       return htmlString;
     }
