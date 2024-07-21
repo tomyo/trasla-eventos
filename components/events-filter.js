bec1a3b64d81e9a72ec2c048158db156d09423c2
tomyo <tomyhayes@gmail.com>
2024-07-21 16:45:05 -0300

    Implement filters and improve hero section (introduce it)

---
+++ components/events-filter.js
@@ -0,0 +1,85 @@
+customElements.define(
+  "events-filter",
+  class extends HTMLElement {
+    constructor() {
+      super();
+      this.addEventListener("input", this);
+      this.events = document.querySelector("#events");
+      this.form = this.querySelector("form");
+
+      // Detect events changes and update hide/show them according to the filters.
+      const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (mutation.type === "childList") {
+            showHideEvents(this.events, [...new FormData(this.form)]);
+          }
+        });
+
+        this.updateLocalitiesOptions(
+          this.events,
+          this.form.querySelector("[name=locality]")
+        );
+      });
+      observer.observe(this.events, { childList: true });
+    }
+
+    handleEvent(event) {
+      if (event.type === "input") {
+        showHideEvents(this.events, [...new FormData(this.form)]);
+      }
+    }
+
+    updateLocalitiesOptions(events) {
+      const availableLocalities = new Map();
+      events.querySelectorAll("event-entry").forEach((event) => {
+        let locality = event.dataset.locality;
+        let localityCount = availableLocalities.has(locality)
+          ? availableLocalities.get(locality)
+          : 0;
+        availableLocalities.set(event.dataset.locality, localityCount + 1);
+      });
+      for (const option of this.form.querySelector("[name=locality]").options) {
+        if (!availableLocalities.has(option.value)) {
+          option.hidden = true;
+          continue;
+        }
+        option.textContent = `${option.value} (${availableLocalities.get(
+          option.value
+        )})`;
+        option.hidden = false;
+      }
+    }
+  }
+);
+
+function showHideElement(element, criteriaFormData) {
+  let shouldHide = false;
+  for (const [key, value] of criteriaFormData) {
+    // Filter by locality
+    if (key === "locality") {
+      if (!element.dataset.locality.includes(value)) {
+        shouldHide = true;
+      }
+    }
+    // Filter by start date
+    if (key == "startDate") {
+      if (element.startDate < new Date(value)) {
+        shouldHide = true;
+      }
+    }
+  }
+  if (shouldHide) return element.toggleAttribute("hidden", true);
+
+  element.removeAttribute("hidden");
+}
+
+/**
+ *
+ * @param {[HTMLElement]} events
+ * @param {FormData} criteriaFormData
+ */
+function showHideEvents(events, criteriaFormData) {
+  events.querySelectorAll("event-entry").forEach((event) => {
+    showHideElement(event, criteriaFormData);
+  });
+}
