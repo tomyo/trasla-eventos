3e1ea16556c044264c0d05cd5b2d7044f25f4509
tomyo <tomyhayes@gmail.com>
2024-07-24 01:22:24 -0300

    Little fixes to improve page score

--- components/event-entry.js
+++ components/event-entry.js
@@ -96,7 +96,7 @@ customElements.define(
           <a part="button" target="_blank" title="Contactarse por este evento" href="https://api.whatsapp.com/send?phone=${formatPhoneNumber(
             this.dataset.phone
           )}&text=${encodeURI(helloMsg)}">
-            <img src="/assets/icons/whatsapp-256w.png" height="100%" alt="WhatsApp" loading="lazy"/>
+            <img src="/assets/icons/whatsapp-256w.png" height="21" alt="WhatsApp" loading="lazy"/>
           </a>
         `;
       }
@@ -108,7 +108,7 @@ customElements.define(
             "@",
             ""
           )}">
-            <img src="/assets/icons/instagram-240w.png" height="100%" alt="Instagram" loading="lazy"/>
+            <img src="/assets/icons/instagram-240w.png" height="21" alt="Instagram" loading="lazy"/>
           </a>
         `;
       }
@@ -124,15 +124,17 @@ customElements.define(
 
         htmlString += /*html*/ `
           <a part="button" target="_blank" href=${href} title="Como llegar">
-            <img src="/assets/icons/google-maps-256w.png" height="100%" alt="Google Maps" loading="lazy"/>
+            <img src="/assets/icons/google-maps-256w.png" height="21" alt="Google Maps" loading="lazy"/>
           </a>
         `;
       }
 
       // Add Google Calendar button
-      htmlString += /*html*/ `<a target="_blank" part="button"  title="Agregar a tu calendario"  href="${createGoogleCalendarUrl(
-        this
-      )}"><img src="/assets/icons/google-calendar.svg" height="100%"></a>`;
+      htmlString += /*html*/ `
+        <a target="_blank" part="button"  title="Agregar a tu calendario"
+            href="${createGoogleCalendarUrl(this)}">
+          <img src="/assets/icons/google-calendar.svg" height="21" alt="">
+        </a>`;
 
       // Add link to share the event
       const slug = slugify(
@@ -143,8 +145,8 @@ customElements.define(
       const shareUrl = `${location.origin}/${encodeURI(slug)}`;
       htmlString += /*html*/ `
         <share-url data-action="share" data-fallback-action="clipboard" data-text-success="Compartido" data-text-success-fallback="Link copiado" data-url="${shareUrl}" title="${shareUrl}">
-          <button part="button">
-            <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg>
+          <button part="button" Argentina>
+            <svg xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg>
           </button>
         </share-url>`;
 
