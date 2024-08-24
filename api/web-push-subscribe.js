d54c27432cdd2c0e2491733f06621d710a899a25
tomyo <tomyhayes@gmail.com>
2024-08-24 03:10:27 -0300

    test changing response to json

--- api/web-push-subscribe.js
+++ api/web-push-subscribe.js
@@ -5,7 +5,6 @@ const vapidKeys = {
   privateKey: process.env.VAPID_KEY_PRIVATE,
 };
 
-console.log("VAPID Keys", vapidKeys, process.env);
 webPush.setVapidDetails("mailto:info@trasla.com.ar", vapidKeys.publicKey, vapidKeys.privateKey);
 
 export async function POST(request) {
@@ -13,9 +12,10 @@ export async function POST(request) {
    * @type {PushSubscription}
    */
   const pushSubscription = await request.json();
-  const result = webPush.sendNotification(pushSubscription, "Your Push Payload Text");
+  const data = JSON.stringify({ some: "data" });
+  const result = await webPush.sendNotification(pushSubscription, "Your Push Payload Text");
   console.log("Push Result", result);
-  return new Response(JSON.stringify(result));
+  return new Response(JSON.stringify({ ok: true }));
 }
 
 function createVapidKeys() {
