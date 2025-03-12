0937aa6b1576c89b8cadc134dbff539b1b0e197f
tomyo <tomyhayes@gmail.com>
2025-03-12 00:49:43 -0300

    share_target test implementation

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,15 @@
-const APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw_0-QeAq8wbM75_YCwEsKHXAEICKQT3f9gLRo3PmM/dev";
-
+// Firefox doesn't support import as es6 modules yet.
+const SHARE_TARGET_ACTION = "/share-target"; // Also used as the cache key for the shared data
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method === "POST" && event.request.url.includes("/share-target")) {
+  const url = new URL(event.request.url);
+  if (event.request.method === "POST" && url.pathname === SHARE_TARGET_ACTION) {
+    console.info("Handling share target:", event.request);
     return event.respondWith(handleShareTarget(event.request));
   }
-  console.info("Skipping unrecognized fetch event", event);
 });
 
+// UTILS
 
 function readAsDataURL(file) {
   return new Promise((resolve, reject) => {
@@ -23,45 +25,27 @@ function readAsDataURL(file) {
  * @returns {Promise<Response>} - A promise that resolves to a redirect response or an error response if sharing fails.
  */
 async function handleShareTarget(request) {
-  console.info(request);
-  try {
-    const formData = await request.formData();
-    const params = new URLSearchParams();
-    console.info("Received formData: ", [...formData.entries()]);
-    // Standard fields
-    ['title', 'text', 'url'].forEach(field => {
-      params.append(field, formData.get(field) || '');
-    });
+  const formData = await request.formData();
+  console.info("[handleShareTarget] received formData: ", [...formData.entries()]);
 
-    // Send files as base64 encoded strings
-    for (const file of formData.getAll('files')) {
-      if (file.size === 0) {
-        console.warn('Skipping empty file:', file.name);
-        continue;
-      }
-      // Append to params (creates multiple "files" entries)
-      params.append('files', await readAsDataURL(file));
+  // Update cache so it's available for preview in the app
+  const cache = await caches.open(SHARE_TARGET_ACTION);
+  // Clear previous cache entries
+  const keys = await cache.keys();
+  await Promise.all(keys.map((key) => cache.delete(key)));
+
+  // Cache all formData
+  let filesCount = 0; // To make a unique cache key for each file
+  for (let [key, value] of formData.entries()) {
+    console.log("caching", key, value);
+    if (key === "files") {
+      key = `/file-${filesCount}`;
+      filesCount += 1;
     }
-
-    const response = await fetch(APP_SCRIPT_URL, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      body: params
-    });
-
-    const json = await response.json();
-    console.info('Share response:', json);
-    if (json.status == 303) return Response.redirect(json.location, 303); 
-
-    
-    return Response.redirect(
-      `/publicar-evento?fileId=${result?.fileId}&description=${encodeURIComponent(text)}`,
-      303
-    );
-  } catch (error) {
-    console.error('Sharing failed:', error);
-    return new Response('Share failed', {status: 500});
+    await cache.put(key, new Response(value));
   }
+
+  return Response.redirect(`/publicar-evento.html`, 303);
 }
 
 // Notifications
@@ -77,4 +61,10 @@ self.addEventListener("push", (event) => {
   };
 
   event.waitUntil(self.registration.sendNotification(data.title ?? "Recordatorio de evento", options));
+});
+
+// PWA INSTALL/CACHE handling
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting(); // Activate worker immediately after installation
 });
