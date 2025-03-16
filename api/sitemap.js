de75b70fd3f3258343b72c7e2055655f6a05236b
tomyo <tomyhayes@gmail.com>
2025-03-15 21:08:44 -0300

    Add past events to the sitemap

--- api/sitemap.js
+++ api/sitemap.js
@@ -2,7 +2,7 @@ import { getSheetData } from "./shared/lib/get-gsheet-data.js";
 
 export default async function handler(req) {
   const url = new URL(req.url);
-  const events = await getSheetData({ includePastEvents: false });
+  const events = await getSheetData({ includePastEvents: true });
 
   let xml = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
