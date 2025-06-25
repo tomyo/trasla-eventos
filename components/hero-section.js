4854854b0105f32896fd31a162609afa2d9869f8
tomyo <tomyhayes@gmail.com>
2025-06-25 10:38:56 -0300

    Implement hero section as hero-section component

---
+++ components/hero-section.js
@@ -0,0 +1,50 @@
+class HeroSection extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = /*html*/ `
+      <!-- Hero section for Trasla Eventos -->
+        <a class="logo-title" href="/" aria-label="Trasla Eventos">
+          <svg-mask style="--src: url('/assets/icons/trasla-eventos.svg'); --color: var(--color-red); height: 40px">
+          </svg-mask>
+        </a>
+        <h1 class="moto">La agenda cultural de Traslasierra</h1>
+        <style>
+          hero-section {
+            display: flex;
+            flex-flow: column;
+            text-align: center;
+            height: 63vh;
+            height: 63lvh;
+            justify-content: start;
+            align-items: center;
+            padding-top: 5rem;
+            color: var(--color-light);
+
+            & .logo-title {
+              display: inline-block;
+              width: 9rem;
+              color: var(--color-red);
+            }
+
+            & .moto {
+              line-height: 1.3;
+              font-weight: 500;
+              font-size: calc(1.3rem + 1vw);
+              font-family: var(--font-family-branding);
+              min-height: 6ch;
+              letter-spacing: 1px;
+              margin-block: auto;
+            }
+
+            & #actions {
+              display: grid;
+              justify-content: center;
+              gap: 1rem;
+              text-align: center;
+            }
+          }
+        </style>
+    `;
+  }
+}
+customElements.define("hero-section", HeroSection);
