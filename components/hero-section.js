88b5262eb2d20330948759484b378e92ee728bd5
tomyo <tomyhayes@gmail.com>
2025-06-29 18:23:02 -0300

    Refactor hero section component and styles for improved layout and structure

--- components/hero-section.js
+++ components/hero-section.js
@@ -1,49 +1,24 @@
 class HeroSection extends HTMLElement {
   constructor() {
-    super();
-    this.innerHTML = /*html*/ `
+    super().attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = /*html*/ `
       <!-- Hero section for Trasla Eventos -->
-        <a class="logo-title" href="/" aria-label="Trasla Eventos">
-          <svg-mask style="--src: url('/assets/icons/trasla-eventos.svg'); --color: var(--color-red); height: 40px">
+        <slot name="logo"></slot>
+        <slot name="title"></slot>
+        <slot></slot>
+    `;
+  }
+
+  connectedCallback() {
+    // Default logo if no logo slot is provided
+    if (this.shadowRoot.querySelector("slot[name='logo']").assignedNodes().length === 0) {
+      this.innerHTML += /*html*/ `
+        <a slot="logo"  href="/" aria-label="Trasla Eventos">
+          <svg-mask style="--src: url('/assets/icons/trasla-eventos.svg'); --color: var(--hero-logo-color, var(--color-red)); height: 40px">
           </svg-mask>
         </a>
-        <h1 class="moto">La agenda cultural de Traslasierra</h1>
-        <style>
-          hero-section {
-            display: flex;
-            flex-flow: column;
-            text-align: center;
-            height: 45vh;
-            height: 45lvh;
-            justify-content: space-around;
-            align-items: center;
-            padding-block: 2rem 1rem;
-            color: var(--color-light);
-
-            & .logo-title {
-              display: inline-block;
-              width: 9rem;
-              color: var(--color-red);
-            }
-
-            & .moto {
-              line-height: 1.3;
-              font-weight: 500;
-              font-size: calc(1.3rem + 1vw);
-              font-family: var(--font-family-branding);
-              min-height: 6ch;
-              letter-spacing: 1px;
-            }
-
-            & #actions {
-              display: grid;
-              justify-content: center;
-              gap: 1rem;
-              text-align: center;
-            }
-          }
-        </style>
-    `;
+      `;
+    }
   }
 }
 customElements.define("hero-section", HeroSection);
