17c499add4747e4e81dfbf805724604bbba58991
tomyo <tomyhayes@gmail.com>
2025-12-02 19:11:24 -0300

    Add eventos-<time> pages to sitemap

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,5 +1,5 @@
 import { getGoogleSheetEvents } from "./shared/lib/get-events.js";
-import { slugify } from "./shared/lib/utils.js";
+import { slugify, getEventSortOrder } from "./shared/lib/utils.js";
 
 let sheetId = typeof process !== "undefined" ? process.env?.GOOGLE_SHEET_ID : undefined;
 let sheetGid = typeof process !== "undefined" ? process.env?.ALL_EVENTS_GOOGLE_SHEET_GID : undefined;
@@ -7,7 +7,7 @@ export default async function handler(req) {
   const url = new URL(req.url);
   const events = await getGoogleSheetEvents(sheetId, sheetGid);
   events.sort((a, b) => {
-    return Number(new Date(b.startsAt)) - Number(new Date(a.startsAt)); // descending order
+    return getEventSortOrder(b) - getEventSortOrder(a); // descending order
   });
 
   let xml = `<?xml version="1.0" encoding="UTF-8"?>
@@ -22,12 +22,19 @@ export default async function handler(req) {
         <priority>1.0</priority>
       </url>`;
 
-  const localitiesInFutureEvents = [];
+  const localitiesInEvents = [];
   let localitiesPagesXml = "";
+  let timeBasedPagesXml = "";
   let eventsXml = "";
+  let newestEventUpdatedAt;
+  const now = new Date();
 
   events.forEach((event) => {
-    const isPastEvent = new Date(event.startsAt) <= new Date();
+    const isPastEvent = new Date(event.startsAt) <= now;
+    const updatedAt = new Date(event.updatedAt);
+    if (updatedAt > newestEventUpdatedAt || !newestEventUpdatedAt) {
+      newestEventUpdatedAt = updatedAt;
+    }
     eventsXml += `
             <url>
               <loc>${url.origin}/${event.slug}</loc>
@@ -36,8 +43,9 @@ export default async function handler(req) {
               <priority>${isPastEvent ? "0.4" : "0.8"}</priority>
             </url>
           `;
-    if (!isPastEvent && !localitiesInFutureEvents.includes[event.locality]) {
-      localitiesInFutureEvents.push(event.locality);
+    if (!localitiesInEvents.includes(event.locality)) {
+      // Add locality page to sitemap if it doesn't exist
+      localitiesInEvents.push(event.locality);
       localitiesPagesXml += `
             <url>
               <loc>${url.origin}/lugar/${slugify(event.locality)}/</loc>
@@ -49,7 +57,18 @@ export default async function handler(req) {
     }
   });
 
-  xml += mainPage + localitiesPagesXml + eventsXml + "</urlset>";
+  for (const when of ["hoy", "esta-semana", "este-mes"]) {
+    timeBasedPagesXml += `
+            <url>
+              <loc>${url.origin}/eventos-${when}/</loc>
+              <lastmod>${newestEventUpdatedAt?.toISOString() || lastModNow}</lastmod>
+              <changefreq>${when === "hoy" ? "hourly" : "daily"}</changefreq>
+              <priority>0.9</priority>
+            </url>
+          `;
+  }
+
+  xml += mainPage + timeBasedPagesXml + localitiesPagesXml + eventsXml + "</urlset>";
 
   const oneMinute = 60;
   return new Response(xml, {
