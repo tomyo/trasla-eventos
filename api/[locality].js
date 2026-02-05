d72ab7058e4e58bda2774309df13f42791b1f0bc
tomyo <tomyhayes@gmail.com>
2026-02-05 10:40:09 -0300

    Move makeCssToHideAbsentLocalitiesInFooter css below ld+json blocks on locality pages

--- api/[locality].js
+++ api/[locality].js
@@ -23,61 +23,63 @@ export default async function handler(req) {
   let html = await (await fetch(`${url.origin}/index.html`)).text();
 
   const contentMeta = /*html*/ `
-      <title>Próximos eventos en ${locality} | TRASLA EVENTOS</title>
-      <link
-        rel="canonical"
-        property="og:url"
-        href="${url.origin}${url.pathname}"
-      />
-      <meta property="og:title" content="Próximos eventos en ${locality}" />
-      <meta
-        name="description"
-        property="og:description"
-        content="¿Qué hacer en ${locality}? Información actualizada de todos los eventos en ${locality} de hoy y de la semana."
-      />
+    <title>Próximos eventos en ${locality} | TRASLA EVENTOS</title>
+    <link
+      rel="canonical"
+      property="og:url"
+      href="${url.origin}${url.pathname}"
+    />
+    <meta property="og:title" content="Próximos eventos en ${locality}" />
+    <meta
+      name="description"
+      property="og:description"
+      content="¿Qué hacer en ${locality}? Información actualizada de todos los eventos en ${locality} de hoy y de la semana."
+    />
 
-      <meta
-        property="og:image"
-        content="https://eventos.trasla.com.ar/assets/images/og-image-1200w-900h.avif"
-      />
-      <meta property="og:image:type" content="image/avif" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="900" />
-      <meta property="og:image" content="https://eventos.trasla.com.ar/assets/images/og-image-1200w-900h.jpg" />
-      <meta property="og:image:type" content="image/jpeg" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="900" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="${url.origin}${url.pathname}" />
+    <meta
+      property="og:image"
+      content="https://eventos.trasla.com.ar/assets/images/og-image-1200w-900h.avif"
+    />
+    <meta property="og:image:type" content="image/avif" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="900" />
+    <meta property="og:image" content="https://eventos.trasla.com.ar/assets/images/og-image-1200w-900h.jpg" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="900" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="${url.origin}${url.pathname}" />
 
-      <meta property="og:site_name" content="TRASLA EVENTOS" />
-      <meta property="og:locale" content="es-AR" />
-      <style>
-        /* hide locality selector */
-        events-filter form select-locality {
-            display: none;
-          }
+    <meta property="og:site_name" content="TRASLA EVENTOS" />
+    <meta property="og:locale" content="es-AR" />
 
-        events-filter form span:has(~ select-locality) {
-          display: none;
-        }
+    <script type="application/ld+json">
+      ${JSON.stringify(localityToSchemaOrgItem(locality, url.origin))}
+    </script>
+    ${
+      filteredEvents.length
+        ? /*html*/ `
+          <script type="application/ld+json">
+            ${JSON.stringify(eventsToSchemaOrgItemList(filteredEvents, url.origin))}
+          </script>
+        `
+        : ""
+    }
 
-        /* Hide each locality link in footer when locality doesn't have events to show */
-        ${makeCssToHideAbsentLocalitiesInFooter(events)}
-      </style>
-      <script type="application/ld+json">
-        ${JSON.stringify(localityToSchemaOrgItem(locality, url.origin))}
-      </script>
-      ${
-        filteredEvents.length
-          ? /*html*/ `
-            <script type="application/ld+json">
-              ${JSON.stringify(eventsToSchemaOrgItemList(filteredEvents, url.origin))}
-            </script>
-          `
-          : ""
+    <style>
+      /* hide locality selector */
+      events-filter form select-locality {
+        display: none;
       }
-    `;
+
+      events-filter form span:has(~ select-locality) {
+        display: none;
+      }
+
+      /* Hide each locality link in footer when locality doesn't have events to show */
+      ${makeCssToHideAbsentLocalitiesInFooter(events)}
+    </style>
+  `;
 
   const contentMetaRegex = /<!-- START CONTENT_METADATA_BLOCK -->[\s\S]*?<!-- END CONTENT_METADATA_BLOCK -->/;
   html = html.replace(contentMetaRegex, contentMeta);
